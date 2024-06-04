81851e7da8cfcc5ec41448e1d918279462e17015
Crowdbotics <team@crowdbotics.com>
2024-06-04 17:21:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "healthyhangout-48259",
+  url: "https://healthyhangout-48259.botics.co",
+  api: "https://healthyhangout-48259.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
